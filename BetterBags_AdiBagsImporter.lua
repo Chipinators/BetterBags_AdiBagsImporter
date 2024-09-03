70e68e52b815fbf380608bda9ef328176a01a0ca
Chris Potter <christopherpotter15@gmail.com>
2024-09-03 09:07:22 -0400

    Fix versioning

--- BetterBags_AdiBagsImporter.lua
+++ BetterBags_AdiBagsImporter.lua
@@ -1,5 +1,5 @@
-local VERSION_INFO = "1.0.6";
-local VERSION_DATE = 1723900152;
+local VERSION_INFO = "1.0.8";
+local VERSION_DATE = 1725368747;
 
 ---@diagnostic disable: missing-fields
 ---@class BetterBags: AceAddon
